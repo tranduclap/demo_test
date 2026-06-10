3be473789c9804b78370087ad866f1f451cc539e
Trần Đức Lập <lapdz1969@gmail.gmailcom>
2026-06-10 11:19:08 +0700

    add json file

---
+++ data.js
@@ -0,0 +1,52 @@
+[
+    {
+        "id": 1,
+        "name": "iPhone 18 Pro Max",
+        "category": "Điện thoại",
+        "price": 35999000,
+        "status": "Còn hàng"
+    },
+    {
+        "id": 2,
+        "name": "Samsung Galaxy Z Fold 6",
+        "category": "Điện thoại",
+        "price": 38499000,
+        "status": "Còn hàng"
+    },
+    {
+        "id": 3,
+        "name": "Laptop acer",
+        "category": "Máy tính xách tay",
+        "price": 14990000,
+        "status": "Hết hàng"
+    },
+    {
+        "id": 4,
+        "name": "Tai nghe jbl",
+        "category": "Tai nghe",
+        "price": 7990000,
+        "status": "Còn hàng"
+    },
+    {
+        "id": 5,
+        "name": "Bàn phím cơ",
+        "category": "Thiết bị",
+        "price": 2099000,
+        "status": "Hết hàng"
+    },
+    {
+        "id": 6,
+        "name": "Chuột không dây",
+        "category": "Thiết bị",
+        "price": 450000,
+        "status": "Còn hàng"
+    },
+    {
+        "id": 7,
+        "name": "Loa bluetooth",
+        "category": "Âm thanh",
+        "price": 350000,
+        "status": "Hết hàng"
+    },
+   
+]
